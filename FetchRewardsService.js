a1e1950388ecfccd60bff326c022a7e938d38ce1
Zhandos Sembay <zsembay8@uab.edu>
2022-05-04 01:30:58 -0500

    Updated FetchRewardsService.js

---
+++ FetchRewardsService.js
@@ -0,0 +1,21 @@
+const express = require("express"),
+    app = express(),
+    port = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
+
+//Use bodyParser()
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+
+global.myPoints = [];
+
+var pointRouter = require("./API /route");
+app.use("/", pointRouter);
+
+app.listen(port);
+
+console.log("WEB service started on " + port + "port");
